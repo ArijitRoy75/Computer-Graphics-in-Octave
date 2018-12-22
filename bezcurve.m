ed709a238c8141366591ed1a5458788d0ed6f3c0
Arijit Ray <35370469+ArijitRoy75@users.noreply.github.com>
2018-12-22 23:37:10 +0530

    Adding Comet Plot as an output

--- bezcurve.m
+++ bezcurve.m
@@ -2,9 +2,13 @@
 %Function cubez() takes a 2x4 matrix as an input and genenerates a 2x101 matrix (p) as an output.
 %The first row of the input and output matrix contains the abcissa of the coordinates and the second row contains the ordinates of the coordinates.
 %The cubez() function generates 99 points in between terminal control points.
-%Try the sample input a=[100, 200, 300, 400; 100, 50, 320, 100]
+%A=[100, 200, 300, 400; 100,50,320,100]
 function p=cubez(points)
   if(size(points)==[2,4])
+    xmin=min(points(1,:))
+    xmax=max(points(1,:))
+    ymin=min(points(2,:))
+    ymax=max(points(2,:))
     p=zeros(2,101)
     bez=zeros(1,4)
     for i =0:100
@@ -14,7 +18,10 @@ function p=cubez(points)
         p(:,i+1)=p(:,i+1)+points(:,j+1).*bez(:,j+1)
       endfor
     endfor
-    plot(p(1,:),p(2,:),points(1,:),points(2,:))
+    hold on
+    plot(points(1,:),points(2,:))
+    comet(p(1,:),p(2,:),0.01);
+    hold off
   else
     printf("Invalid Anguments. Argument matrix must be a 2x4")
   endif
