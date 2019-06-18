0937a552d09e57cd1c7c2b4ebde26953abe1d61a
Arijit Ray <35370469+ArijitRoy75@users.noreply.github.com>
2019-06-18 20:56:10 +0530

    Code Optimization v.1

--- bezcurve.m
+++ bezcurve.m
@@ -3,19 +3,19 @@
 %The first row of the input and output matrix contains the abcissa of the coordinates and the second row contains the ordinates of the coordinates.
 %The cubez() function generates 99 points in between terminal control points.
 %A=[100, 200, 300, 400; 100,50,320,100]
-function p=cubez(points)
+function p=bezcurve(points)
   if(size(points)==[2,4])
-    p=zeros(2,101)
-    bez=zeros(1,4)
-    for i =0:100
-      u=0.01*i
+    p=zeros(2,101);
+    bez=zeros(1,4);
+    for i =0:100;
+      u=0.01*i;
       for j = 0:3
-        bez(:,j+1)=nchoosek(3,j)*power(u,j)*power(1-u,3-j)
-        p(:,i+1)=p(:,i+1)+points(:,j+1).*bez(:,j+1)
+        bez(:,j+1)=nchoosek(3,j)*power(u,j)*power(1-u,3-j);
+        p(:,i+1)=p(:,i+1)+points(:,j+1).*bez(:,j+1);
       endfor
     endfor
-    hold on
-    plot(points(1,:),points(2,:))
+    hold on;
+    plot(points(1,:),points(2,:));
     comet(p(1,:),p(2,:),0.01);
     hold off
   else
